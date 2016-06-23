c451b93150fbe37cf598cb4c1835d3a4a3ac94e2
Josera89 <jramon.zu@gmail.com>
2016-06-23 19:10:20 +0000

    forEach function

--- findWaldo.js
+++ findWaldo.js
@@ -1,17 +1,16 @@
 // The second argument/parameter is expected to be a function
-var arrayposition = [];
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      arrayposition = arr.indexOf("Waldo");
-      found();   // execute callback
+  arr.forEach(function(name, index, arr) {
+    if (name == "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  })
+
 }
 
-function actionWhenFound(arr) {
-  console.log("Found him at position " + arrayposition);
+function actionWhenFound(position) {
+  console.log("Found him at position " + position);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
